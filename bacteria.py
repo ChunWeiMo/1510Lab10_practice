830e40ae52b76557d7b63d6bfe6c68cfc7aa9350
Chun-Wei, Mo <chunwei.mo@gmail.com>
2023-11-27 23:01:19 -0800

    Complete basic depth-first search

--- bacteria.py
+++ bacteria.py
@@ -1,23 +1,65 @@
-def colonize(width, height, x_coordinate, y_coordinate):
+def colonize(width, height, x_coordinate, y_coordinate, max_generation):
     surface = dict()
     for column in range(width):
         for row in range(height):
             surface[(column, row)] = False
-    print(surface)
+    spread_like_bacteria(surface, (x_coordinate, y_coordinate), max_generation)
+    # print(surface)
+    return surface
 
 
-def spread_like_bacteria(surface, coordinate: tuple):
+def spread_like_bacteria(surface, coordinate, max_generation):
+    """
+
+    """
     try:
         surface[coordinate]
     except KeyError:
         print('The coordinate is outside of the surface.')
-    else:
+        return
+
+    if not surface[coordinate] and max_generation >= 0:
         surface[coordinate] = True
-    
-    
+        spread_like_bacteria(
+            surface, (coordinate[0], coordinate[1]-1), max_generation-1)
+        spread_like_bacteria(
+            surface, (coordinate[0], coordinate[1]+1), max_generation-1)
+        spread_like_bacteria(
+            surface, (coordinate[0]-1, coordinate[1]), max_generation-1)
+        spread_like_bacteria(
+            surface, (coordinate[0]+1, coordinate[1]), max_generation-1)
+
+
+def check_populated():
+    """
+    11. You may wish to create a helper function that accepts a reference to the surface and a proposed set of coordinates 
+    returns False if the coordinates are valid and/or already populated
+    returns True if the location is ripe for colonization.
+    """
+    pass
+
+
+def print_surface(surface):
+    width, height = 0, 0
+    for coordinate in surface:
+        if coordinate[0] > width:
+            width = coordinate[0]            
+        if coordinate[1] > height:
+            height = coordinate[1]
+            
+    for row in range(height):
+        for column in range(width):
+            if surface[(column, row)]:
+                print('*', end='')
+            else:
+                print('0', end='')
+        print()
+
+
 def main():
-    colonize(7, 3, 1, 1)
-  
+    surface = colonize(11, 11, 5, 5, 3)
+    print_surface(surface)
+
 
 if __name__ == '__main__':
     main()
